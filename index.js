d1bb5fcbd48428b0dd4083b0dff2a1c9baa56657
przemczan <przemczan@gmail.com>
2014-10-28 23:33:20 +0100

    database, ws server and http server basic error handlers

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 var Config = require('./config.js');
 
-var url = require('url');
-var util = require('util');
-var http = require("http");
 var DB = require("./lib/database").connect(Config.mongo.uri);
-var Model = require('./model');
 
 var wsServer = require('./lib/wsServer');
 var httpServer = require('./lib/httpServer');
 
-wsServer.run(Config.wsServer.port);
+DB.getConnection().on('open', function() {
+    wsServer.run(Config.wsServer.port);
 
-httpServer.onMessage = function(_message) {
-    wsServer.handleIncomingMessage(_message);
-};
+    httpServer.onMessage = function(_message) {
+        wsServer.handleIncomingMessage(_message);
+    };
 
-httpServer.run(Config.httpServer.port);
+    httpServer.run(Config.httpServer.port);
+});
 
