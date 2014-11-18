3e9288752e88e8b3105ab92f11d252dd58a3fcba
Przemysław Telążka <przemyslaw.telazka@pracuj.pl>
2014-11-18 16:00:50 +0100

    web socket response model

--- repository/MessageRepository.js
+++ repository/MessageRepository.js
@@ -4,7 +4,7 @@ var Mongoose = require('mongoose');
 module.exports = {
     getAllForUser: function(_ws, _callback) {
         return Message.find()
-            .where('receivers').in(_ws.data.subscribe)
+            .where('receivers').in(_ws.data.query.subscribe)
             .where('client').equals(Mongoose.Types.ObjectId(_ws.data.client._id))
             .exec(_callback);
     }
